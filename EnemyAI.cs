ae68b75dc8a3e5eacd154e66b1c1297840d3f4ee
sangk95 <49775224+sangk95@users.noreply.github.com>
2019-07-12 13:12:18 +0900

    EnemyDash(final)

--- EnemyAI.cs
+++ EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
     public GameObject Player;
     Vector3 Player_pos;
     Vector3 Enemy_pos;
+    Vector3 dash_pos;
     int speed;
     bool chasing;
     bool dash;
@@ -19,14 +20,25 @@ public class EnemyAI : MonoBehaviour
     }
 
 
-    IEnumerator DelayTime()
+    IEnumerator ChaseDelay()
     {
+        dash_pos = Player_pos;
         yield return new WaitForSeconds(2f);
-        
-        if (Enemy_pos.x - Player_pos.x >= 3 || Enemy_pos.x - Player_pos.x <= -3)
-            chasing = true;
-        else
-            dash = true;
+        dash = true;
+    }
+
+    IEnumerator DashDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        while (Enemy_pos.x - Player_pos.x <= 3 && Enemy_pos.x - Player_pos.x >= -3)
+        {
+            Player_pos = Player.transform.position;
+            Enemy_pos = this.transform.position;
+            if (Enemy_pos.x - Player_pos.x >= 3 || Enemy_pos.x - Player_pos.x <= -3)
+                break;
+            yield return null;
+        }
+        chasing = true;
     }
 
     void Move()
@@ -34,7 +46,7 @@ public class EnemyAI : MonoBehaviour
         if (Enemy_pos.x - Player_pos.x <= 3 && Enemy_pos.x - Player_pos.x >= -3)
         {
             chasing = false;
-            StartCoroutine("DelayTime");
+            StartCoroutine("ChaseDelay");
         }
         else
         {
@@ -53,25 +65,24 @@ public class EnemyAI : MonoBehaviour
     {
         speed = 10;
 
+        
+        if (dash_pos.x < Enemy_pos.x)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (dash_pos.x > Enemy_pos.x)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
 
-        if (Enemy_pos.x - Player_pos.x <= 2 && Enemy_pos.x - Player_pos.x >= -2)
+
+        if (Enemy_pos.x - dash_pos.x <= 2 && Enemy_pos.x - dash_pos.x >= -2)
         {
             speed = 2;
             dash = false;
-            StartCoroutine("DelayTime");
+            StartCoroutine("DashDelay");
         }
-
-        else
-        {
-            if (Player_pos.x < Enemy_pos.x)
-            {
-                transform.Translate(Vector3.left * speed * Time.deltaTime);
-            }
-            else if (Player_pos.x > Enemy_pos.x)
-            {
-                transform.Translate(Vector3.right * speed * Time.deltaTime);
-            }
-        }
+        
     }
     
     private void Update()
